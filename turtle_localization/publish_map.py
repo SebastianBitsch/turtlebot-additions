928d0f9f7b53e166443f937c8a42801cf0c4cc7f
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2024-04-17 02:48:48 +0200

    cleanup

--- turtle_localization/publish_map.py
+++ turtle_localization/publish_map.py
@@ -16,47 +16,6 @@ from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 
-def quaternion_rotation_matrix(Q):
-    """
-    Covert a quaternion into a full three-dimensional rotation matrix.
- 
-    Input
-    :param Q: A 4 element array representing the quaternion (q0,q1,q2,q3) 
- 
-    Output
-    :return: A 3x3 element matrix representing the full 3D rotation matrix. 
-             This rotation matrix converts a point in the local reference 
-             frame to a point in the global reference frame.
-    """
-    # Extract the values from Q
-    q0 = Q[0]
-    q1 = Q[1]
-    q2 = Q[2]
-    q3 = Q[3]
-     
-    # First row of the rotation matrix
-    r00 = 2 * (q0 * q0 + q1 * q1) - 1
-    r01 = 2 * (q1 * q2 - q0 * q3)
-    r02 = 2 * (q1 * q3 + q0 * q2)
-     
-    # Second row of the rotation matrix
-    r10 = 2 * (q1 * q2 + q0 * q3)
-    r11 = 2 * (q0 * q0 + q2 * q2) - 1
-    r12 = 2 * (q2 * q3 - q0 * q1)
-     
-    # Third row of the rotation matrix
-    r20 = 2 * (q1 * q3 - q0 * q2)
-    r21 = 2 * (q2 * q3 + q0 * q1)
-    r22 = 2 * (q0 * q0 + q3 * q3) - 1
-     
-    # 3x3 rotation matrix
-    rot_matrix = np.array([[r00, r01, r02],
-                           [r10, r11, r12],
-                           [r20, r21, r22]])
-                            
-    return rot_matrix
-
-
 # def array2occupancy_grid(map: np.ndarray, map_width: int, map_height: int, map_resolution: float, map_origin: Pose) -> OccupancyGrid:
 def array2occupancy_grid(map: np.ndarray):
     return OccupancyGrid(
@@ -76,7 +35,9 @@ def array2occupancy_grid(map: np.ndarray):
 
 class MapPublisher(Node):
 
-    def __init__(self, map_width: int = 311, map_height: int = 164, publish_frequency: float = 0.5, verbose: bool = False):
+    def __init__(self, map_size: tuple = (164, 311) , publish_frequency: float = 0.5, verbose: bool = False):
+        """ map_size: height, width """
+
         super().__init__('map_publisher')
         self.verbose = verbose
 
@@ -93,11 +54,9 @@ class MapPublisher(Node):
         self.robot_pose = Pose()
 
         # Init map
-        self.width = map_width
-        self.height = map_height
-        self.map_size = np.array([map_height, map_width])
         self.resolution = 0.05
-        self.map = np.zeros((map_height, map_width))
+        self.map_size = np.asarray(map_size)
+        self.map = np.zeros(map_size)
         # self.map = np.random.randint(0, 128, size = (map_height, map_width), dtype="int8")
         # threshold = 100
         # self.map[threshold < self.map] = 127
@@ -125,13 +84,10 @@ class MapPublisher(Node):
         )
         self.odom_subscription  # prevent unused variable warning, not sure needed
 
-    def world_coords2map_coords(self, x, y) -> np.ndarray:
-        return np.floor(np.array([y, x]) / self.resolution + self.map_size / 2.0)#.astype("uint8")
 
-    def transform(self, x: np.ndarray, R: np.ndarray, t: np.ndarray) -> np.ndarray:
-        """ rotate and move a vector, not sure transform is the right word """
-        assert R.shape == (3,3) and t.shape == (3,) and x.shape[1] == 3, "Wrong shapes"
-        return (R @ x.T + t[:, None]).T
+    def world_coords2map_coords(self, x, y) -> np.ndarray:
+        return np.floor(np.array([y, x]) / self.resolution + self.map_size / 2.0).astype(int)
+
 
     def compute_world_coords(self, ranges: np.ndarray, angles: np.ndarray) -> np.ndarray:
         """ Assumes 2D scanner, i.e. (probably) wont work in 3D """
@@ -163,56 +119,27 @@ class MapPublisher(Node):
         # Update the target and current points
         points = self.compute_world_coords(ranges, angles)
 
-        R = quaternion_rotation_matrix(self.robot_rotation)
-
-        # pose_stamped = tf2_geometry_msgs.PoseStamped()
-        # pose_stamped.pose = self.robot_pose
-        # pose_stamped.pose.position.x += 10.0
-        # pose_stamped.header.frame_id = "base_link"
-        # pose_stamped.header.stamp = rclpy.time.Time().to_msg() #self.get_clock().now().to_msg()
-
+        # Get the transform from robot to world
         try:
-            print("------")
             transform = self.tf_buffer.lookup_transform("map", "base_link", rclpy.time.Time().to_msg())
-
-            for i, coord in enumerate(points):
-                point = PointStamped()
-                point.header.frame_id = "base_link"
-                point.point.x = coord[0]
-                point.point.y = coord[1]
-                point.point.z = coord[2]
-                t = do_transform_point(point, transform)
-                # print(t)
-
-                grid_coords = self.world_coords2map_coords(t.point.x, t.point.y)
-                if i == 0:
-                    print(t.point.x, t.point.y, grid_coords, self.map_size)
-                if grid_coords[0] < self.height and grid_coords[1] < self.width:
-                    self.map[int(grid_coords[0]), int(grid_coords[1])] = 100
-                
-            # t = self.tf_buffer.transform(pose_stamped, "map", rclpy.duration.Duration(seconds=1.0))
-            # t = self.tf_buffer.wait_for_transform_async("map", "baselink", self.get_clock().now().to_msg())
-            
-            # print(t)
         except TransformException as ex:
-            self.get_logger().info(
-                f'Could not transform {ex}')
+            self.get_logger().info(f'Could not transform point in mapping: {ex}')
             return
-        
-        #lookup_transform("map", self.target_frame, rclpy.time.Time())
 
+        # Plot all the points
+        for point in points:
+            point = PointStamped(
+                header = Header(frame_id = "base_link"),
+                point = Point(x = point[0], y = point[1], z = point[2])
+            )
+            t = do_transform_point(point, transform)
 
-        # for robo_coords in points:
-        #     world_coords = self.robot_position + robo_coords #  @ np.linalg.inv(R)
+            grid_coords = self.world_coords2map_coords(t.point.x, t.point.y)
 
-        #     grid_coords = self.world_coords2map_coords(world_coords[1], world_coords[0])
-        #     if grid_coords[0] < self.height and grid_coords[1] < self.width:
-        #         self.map[grid_coords[0], grid_coords[1]] = 100
-
-        
-        # print(world_coords[0])
-        
-
+            # Write the point to the grid
+            if (grid_coords < self.map_size).all():
+                self.map[grid_coords[0], grid_coords[1]] = 100
+    
 
     def odometry_callback(self, odometry: Odometry) -> None:
         """ Update the robot position and rotation from the odometry """
