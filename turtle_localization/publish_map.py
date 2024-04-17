73c7d16d59f935cecfb5442b85820edb70d3b399
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2024-04-17 03:08:27 +0200

    more cleanup and refactoring

--- turtle_localization/publish_map.py
+++ turtle_localization/publish_map.py
@@ -8,7 +8,6 @@ from sensor_msgs.msg import LaserScan, PointField, PointCloud2
 from std_msgs.msg import Header
 from geometry_msgs.msg import Pose, Point, Quaternion, PointStamped
 from nav_msgs.msg import OccupancyGrid, Odometry, MapMetaData
-import tf2_geometry_msgs
 from tf2_geometry_msgs import do_transform_point
 
 # Transforms stuff
@@ -16,51 +15,26 @@ from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 
-# def array2occupancy_grid(map: np.ndarray, map_width: int, map_height: int, map_resolution: float, map_origin: Pose) -> OccupancyGrid:
-def array2occupancy_grid(map: np.ndarray):
-    return OccupancyGrid(
-        header = Header(frame_id = "map"), # the reference frame is the "true" map - which is the top level tf
-        info = MapMetaData(
-            resolution = 0.05,
-            width = 311,
-            height = 164,
-            origin = Pose(
-                position = Point(x = -7.79, y = -4.06, z = 0.0), # TODO: How are we supposed to get this ??
-                orientation = Quaternion(x = 0.0, y = 0.0, z = 0.0, w = 1.0)
-            )
-        ),
-        data = map.flatten().astype("int8")
-    )
-
 
 class MapPublisher(Node):
+    """ """
 
-    def __init__(self, map_size: tuple = (164, 311) , publish_frequency: float = 0.5, verbose: bool = False):
+    def __init__(self, map_size: tuple = (164, 311), map_resolution: float = 0.05, publish_frequency: float = 0.5, verbose: bool = False):
         """ map_size: height, width """
 
         super().__init__('map_publisher')
         self.verbose = verbose
-
-        # Declare and acquire `target_frame` parameter
-        # TODO: Dont know why we define it like this
-        self.target_frame = self.declare_parameter('target_frame', 'base_link').get_parameter_value().string_value
     
         self.tf_buffer = Buffer()
-        self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.tf_listener = TransformListener(self.tf_buffer, self) # This has to be set although it isn't used
 
         # State of the robot
-        self.robot_position = np.zeros(3)
-        self.robot_rotation = np.zeros(4)
         self.robot_pose = Pose()
 
         # Init map
-        self.resolution = 0.05
-        self.map_size = np.asarray(map_size)
-        self.map = np.zeros(map_size)
-        # self.map = np.random.randint(0, 128, size = (map_height, map_width), dtype="int8")
-        # threshold = 100
-        # self.map[threshold < self.map] = 127
-        # self.map[self.map <= threshold] = 0
+        self.map_resolution = map_resolution
+        self.map_size = np.array(map_size, dtype=int)
+        self.map = np.zeros(map_size, dtype=int)
 
         # Create a publisher to send map
         self.map_publisher = self.create_publisher(OccupancyGrid, topic = 'map1', qos_profile = 10)
@@ -68,25 +42,39 @@ class MapPublisher(Node):
 
         # Subscribe to the scan message
         self.scan_subscription = self.create_subscription(
-            LaserScan,
-            'scan',
-            self.scan_callback,
-            10
+            msg_type = LaserScan,
+            topic = 'scan',
+            callback = self.scan_callback,
+            qos_profile = 10
         )
-        self.scan_subscription  # prevent unused variable warning, not sure needed
 
         # Subscribe to the odometry message
         self.odom_subscription = self.create_subscription(
-            Odometry,
-            'odom',
-            self.odometry_callback,
-            10
+            msg_type = Odometry,
+            topic = 'odom',
+            callback = self.odometry_callback,
+            qos_profile = 10
         )
-        self.odom_subscription  # prevent unused variable warning, not sure needed
+
+
+    def array2occupancy_grid(self, map: np.ndarray):
+        return OccupancyGrid(
+            header = Header(frame_id = "map"), # the reference frame is the "true" map - which is the top level tf
+            info = MapMetaData(
+                resolution = self.map_resolution,
+                width = int(self.map_size[1]), # TODO: Dont know why we have to cast here, shouldnt be neccesary
+                height = int(self.map_size[0]), # TODO: Dont know why we have to cast here, shouldnt be neccesary
+                origin = Pose(
+                    position = Point(x = -7.79, y = -4.06, z = 0.0), # TODO: How are we supposed to get this ??
+                    orientation = Quaternion(x = 0.0, y = 0.0, z = 0.0, w = 1.0)
+                )
+            ),
+            data = map.flatten().astype("int8")
+        )
 
 
     def world_coords2map_coords(self, x, y) -> np.ndarray:
-        return np.floor(np.array([y, x]) / self.resolution + self.map_size / 2.0).astype(int)
+        return np.floor(np.array([y, x]) / self.map_resolution + self.map_size / 2.0).astype(int)
 
 
     def compute_world_coords(self, ranges: np.ndarray, angles: np.ndarray) -> np.ndarray:
@@ -102,7 +90,7 @@ class MapPublisher(Node):
 
     def publish_map(self):
         """ """
-        self.map_publisher.publish(array2occupancy_grid(self.map))
+        self.map_publisher.publish(self.array2occupancy_grid(self.map))
 
 
     def scan_callback(self, scan: LaserScan) -> None:
@@ -122,8 +110,9 @@ class MapPublisher(Node):
         # Get the transform from robot to world
         try:
             transform = self.tf_buffer.lookup_transform("map", "base_link", rclpy.time.Time().to_msg())
-        except TransformException as ex:
-            self.get_logger().info(f'Could not transform point in mapping: {ex}')
+        except TransformException as exception:
+            if self.verbose:
+                self.get_logger().info(f'Could not transform point in mapping: {exception}')
             return
 
         # Plot all the points
@@ -144,17 +133,7 @@ class MapPublisher(Node):
     def odometry_callback(self, odometry: Odometry) -> None:
         """ Update the robot position and rotation from the odometry """
         self.robot_pose = odometry.pose.pose
-        self.robot_position = np.array([
-            odometry.pose.pose.position.x,
-            odometry.pose.pose.position.y,
-            odometry.pose.pose.position.z    
-        ])
-        self.robot_rotation = np.array([
-            odometry.pose.pose.orientation.x,
-            odometry.pose.pose.orientation.y,
-            odometry.pose.pose.orientation.z,
-            odometry.pose.pose.orientation.w
-        ])
+
 
 def main(args=None):
     rclpy.init(args=args)
