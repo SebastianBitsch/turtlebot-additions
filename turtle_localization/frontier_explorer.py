10bf0344ea7847148037a943824321740c5b530d
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2024-04-30 20:40:50 +0200

    cleanup and fix bugs, still not perfect

--- turtle_localization/frontier_explorer.py
+++ turtle_localization/frontier_explorer.py
@@ -1,6 +1,3 @@
-from enum import IntEnum
-from collections import deque
-
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
@@ -11,131 +8,124 @@ import numpy as np
 from sensor_msgs.msg import LaserScan, PointField, PointCloud2
 from std_msgs.msg import Header
 from geometry_msgs.msg import Pose, Point, Quaternion, PoseStamped
-from nav_msgs.msg import OccupancyGrid, Odometry, MapMetaData
-from tf2_geometry_msgs import do_transform_point
-
-# Transforms stuff
-from tf2_ros import TransformException
-from tf2_ros.buffer import Buffer
-from tf2_ros.transform_listener import TransformListener
+from nav_msgs.msg import OccupancyGrid
 
 
 class FrontierPublisher(Node):
     """
     """
 
-    def __init__(self, publish_frequncy:float = 0.1):
-        """ map_size: height, width """
+    def __init__(self):
+        """ """
 
         super().__init__('frontier_publisher')
 
         # Subscribe to the map
-        # self.frontier_subscription = self.create_subscription(
-        #     msg_type = OccupancyGrid,
-        #     topic = 'frontier',
-        #     callback = self.frontier_callback,
-        #     qos_profile = 10
-        # )
+        self.frontier_subscription = self.create_subscription(
+            msg_type = OccupancyGrid,
+            topic = 'frontier',
+            callback = self.frontier_callback,
+            qos_profile = 10
+        )
 
-        # self.navigation_goal_publisher = self.create_publisher(
-        #     OccupancyGrid, 
-        #     topic = 'frontier', 
-        #     qos_profile = 10
-        # )
-        # self.timer = self.create_timer(publish_frequncy, self.publish_navigation_goal)
-        self.navigation_action_client = ActionClient(self, PoseStamped, 'navigate_to_pose')
+        self.has_goal = False   # Used for setting the first goal
+        self.map = None
+        self.navigation_action_client = ActionClient(self, NavigateToPose, 'navigate_to_pose')
 
 
-    def send_goal(self, pose):
-        pose.header.frame_id = "map"
-        # self.coord2pose(np.array([0.0, 0.0]))
+    def send_goal(self):
+        pose = self.calculate_new_pose(self.map)
+        print(f"Sending new goal: {pose.pose.pose.position.x},{pose.pose.pose.position.y}")
+
         self.navigation_action_client.wait_for_server()
-        return self.navigation_action_client.send_goal_async(pose)
+
+        self._send_goal_future = self.navigation_action_client.send_goal_async(pose)
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
 
 
-    # def publish_navigation_goal(self):
-    #     """ """
-    #     if self.goal is not None:
-    #         self.navigation_goal_publisher.publish()# TODO
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            print('Goal rejected')
+            return
+        
+        print('Goal accepted')
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
 
 
-    # def coord2pose(self, coords: np.ndarray) -> Pose:
-    #     """ Turn a 2D coord into a Pose - could be a better function"""
-    #     return PoseStamped(
-    #         header = Header(
-    #         ),
-    #         pose = Pose(
-    #             position = Point(
-    #                 x = coords[0],
-    #                 y = coords[1],
-    #                 z = 0.0,
-    #         ),
-    #             orientation = Quaternion(
-    #                 x = 0.0,
-    #                 y = 0.0,
-    #                 z = 0.0,
-    #                 w = 0.0,
-    #             )
-    #         )
-    #     )
+    def get_result_callback(self, future):
+        """ Called once the goal has finished """
+        result = future.result().result
+        print('Result: {0}'.format(result))
 
-    # def map2world_coords(self, x:int, y:int) -> np.ndarray:
-    #     """
-    #     """
-    #     # map 2 world
-    #     # return np.array([y, x]) * self.map_resolution + self.map_size / 2.0
-    #     return np.array([y, x]) * 0.1 + np.array([82, 155]) / 2.0
+        # Send a new random goal
+        self.send_goal()
+
+
+    def coord2pose(self, coords: np.ndarray) -> Pose:
+        """ Turn a 2D coord into a Pose - could be a better function"""
+        goal_msg = NavigateToPose.Goal()
+        goal_msg.pose.pose.position.x = float(coords[0])
+        goal_msg.pose.pose.position.y = float(coords[1])
+        goal_msg.pose.pose.position.z = 0.0
+        goal_msg.pose.pose.orientation.x = 0.0
+        goal_msg.pose.pose.orientation.y = 0.0
+        goal_msg.pose.pose.orientation.z = 0.0
+        goal_msg.pose.pose.orientation.w = 0.0
+        goal_msg.pose.header.frame_id = "map"   # The world coordinate frame
+        return goal_msg
+
+
+    def map2world_coords(self, grid_x, grid_y) -> np.ndarray:        
+        return (np.array([grid_x, grid_y]) - np.array([self.map_width, self.map_height]) / 2.0) * self.map_resolution
 
     
-    # def select_random_grid_coord(self, frontier_grid: np.ndarray) -> tuple:
-    #     """ Select a random 2D coordinate from a boolean grid """
-    #     indices = np.where(frontier_grid)
-    #     random_index = np.random.choice(np.arange(len(indices)), size=1)
-    #     row = indices[1][random_index]
-    #     col = indices[0][random_index]
-    #     return row, col
+    def select_random_grid_coord(self, frontier_grid: np.ndarray) -> tuple:
+        """ Select a random 2D coordinate from a boolean grid """
+        indices = np.where(frontier_grid)
+        random_index = np.random.choice(np.arange(len(indices)), size=1)[0]
+        row = indices[1][random_index]
+        col = indices[0][random_index]
+        return row, col
 
 
-    # def calculate_new_pose(self, frontier_grid: np.ndarray) -> Pose:
-    #     # Could be any method, here we select a random grid coord
-    #     row, col = self.select_random_grid_coord(frontier_grid=frontier_grid)
+    def calculate_new_pose(self, frontier_grid: np.ndarray) -> Pose:
+        # Could be any method, here we select a random grid coord
+        row, col = self.select_random_grid_coord(frontier_grid=frontier_grid)
 
-    #     # Convert from grid coordinate to world coordinate
-    #     world_coords = self.map2world_coords(col, row)
+        # Convert from grid coordinate to world coordinate
+        world_coords = self.map2world_coords(col, row)
 
-    #     return self.coord2pose(world_coords) # TODO: 
+        return self.coord2pose(world_coords)
 
 
-    # def frontier_callback(self, occupancy_grid: OccupancyGrid) -> None:
-    #     """ Take a frontier occupancy grid and give a next goal pose """
+    def frontier_callback(self, occupancy_grid: OccupancyGrid) -> None:
+        """ Take a frontier occupancy grid and give a next goal pose """
+    
+        self.map_width = occupancy_grid.info.width   # 155
+        self.map_height = occupancy_grid.info.height # 82 
+        self.map_resolution = occupancy_grid.info.resolution # 0.1
 
-    #     map_width = occupancy_grid.info.width
-    #     map_height = occupancy_grid.info.height
-
-    #     map = np.array(occupancy_grid.data).reshape(map_height, map_width)
-
-    #     # pose = self.calculate_new_pose(map)
-
-    #     # if not self.has_sent:
-    #     #     print("Sending goal")
-    #     #     self.send_goal()
-    #     #     self.has_sent = True
+        self.map = np.array(occupancy_grid.data).reshape(self.map_height, self.map_width)
+        
+        # First time we get map info we set the goal of the robot
+        if not self.has_goal:
+            self.send_goal()
+            self.has_goal = True
 
 
 def main(args=None):
     rclpy.init(args=args)
 
     frontier_pub = FrontierPublisher()
-    future = frontier_pub.send_goal(PoseStamped())
+    rclpy.spin(frontier_pub)
 
-    rclpy.spin_until_future_complete(frontier_pub, future)
-    # rclpy.spin(frontier_pub)
-
-    # # Destroy the node explicitly
-    # # (optional - otherwise it will be done automatically
-    # # when the garbage collector destroys the node object)
-    # frontier_pub.destroy_node()
-    # rclpy.shutdown()
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    frontier_pub.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
